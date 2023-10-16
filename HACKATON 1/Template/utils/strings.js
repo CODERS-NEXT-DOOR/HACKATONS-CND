22176e592bd44dcf2ba0ec05a2a1d08e73cea15b
Antoniya-A <tonii.asenova@gmail.com>
2023-10-16 17:57:50 +0300

    String function added: trim

--- HACKATON 1/Template/utils/strings.js
+++ HACKATON 1/Template/utils/strings.js
@@ -24,8 +24,31 @@ const split = (string, separator) => {                      //Martin
   // TODO
 };
 
+
+/**
+ * Trims the trailing spaces from the start and the end of a given string.
+ *
+ * @param {string} string the string to trim.
+ * @return {array} the trimmed string.
+ */
 const trim = (string) => {                                 //Toni
-  // TODO
+  let startStr = 0;
+  let endStr = string.length-1;
+  let trimmedStr = '';
+
+  while(string[startStr] === ' '){
+    startStr++
+  }
+
+  while(string[endStr] === ' '){
+    endStr--
+  }
+
+  for(let i = startStr; i<= endStr; i++){
+    trimmedStr += string[i];
+  }
+
+  return trimmedStr;
 };
 
 export { slice, repeat, capitalize, replace, split, trim };
