3fee35e3e94c576410486697979b2038254e6df4
Vyara Delcheva <valentinesbyv@yahoo.com>
2023-10-18 13:40:45 +0300

    add comments to index.js

--- HACKATON 1/Template/index.js
+++ HACKATON 1/Template/index.js
@@ -362,10 +362,10 @@ const expressions = () => {
 // Arrays
 
 // reverse();
-// fill();
+ //fill();                                  //vyara
 // join();                                 //GEORGI
 // indexOf();                             //Martin
-// filter();
+//filter();                                //vyara
 // zip();
 // arrays();
 
@@ -376,12 +376,12 @@ const expressions = () => {
 // average();                            //Andy
 // pow();
 // isPrime();                          //GEORGI
-// swapWholeAndRemainder();
+// swapWholeAndRemainder();            //vyara
 // math();
 
 // Objects
 
-// existInObject();
+// existInObject();                        //vyara
 // removeProp();                           //GEORGI
 // copy();                                //Andy
 // typeOfProps();                        //Martin
@@ -393,7 +393,7 @@ const expressions = () => {
 
 // slice();                                //Martin
 // repeat();                              //Andy
-// capitalize();
+// capitalize();                          //vyara
 // replace();                           //GEORGI
 // split();                            //Martin
 // trim();
@@ -401,9 +401,9 @@ const expressions = () => {
 
 // Expressions
 
-isMinLength();                              //Andy
+//isMinLength();                              //Andy
 // isMaxLength();
-// isIn();
+// isIn();                                   //vyara
 // isArrayOfType();
 // areValidNumbers();                     //GEORGI
 //sumNumbersFromString();               // Andy
